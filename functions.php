a1995ebf2f251687d2f38d92543ac36fc11d88e8
Nikolas Santis <nikosantis@gmail.com>
2019-11-08 12:29:12 -0300

    Comentarios en home.php y functions.php, estoy emocionado

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 //comentario
 require get_template_directory() . '/functions/setup.php';
 require get_template_directory() . '/functions/enqueues.php';
 
+//comentario nuevo de Nikolas
 ?>
